d9c0592d6aaccd9d17c616f58ae2192010f10b98
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-09-06 05:54:47 -0300

    Added react router and sorted out main components

---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const App = () => {
+  return <div>Multictactoe</div>
+}
+
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-const App = () => {
-  return <div>Multictactoe</div>
-}
+import App from './App'
 
-ReactDOM.render(<App />, document.querySelector("#root"))
+import './index.scss'
+
+ReactDOM.render(
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Route path='/' component={App} />
+    </BrowserRouter>, 
+document.getElementById('root'))
