63ed2467c6131df9d714f62831910c3542d80ad4
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-09-06 21:40:22 -0300

    Fix routing error with webpack

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Zoom from 'react-reveal/Zoom'
 
 export default class Login extends Component{
     constructor(props){
@@ -7,6 +8,10 @@ export default class Login extends Component{
     }
 
     render(){
-        return <div />
+        return (
+            <Zoom top>
+                <h1>EAE</h1>
+            </Zoom>
+        )
     }
 }
