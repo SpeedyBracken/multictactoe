1fbeba6f9929fc12234c1ca37c7e8c380a07897d
coutlcdo <coutlcdo@outlook.com>
2019-09-06 23:50:45 -0300

    Simple test

--- api/server.js
+++ api/server.js
@@ -2,22 +2,25 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
-const cors = require('cors')
+// const cors = require('cors')
 
 const testAPIRouter = require("./routes/testAPI.js")
 
-app.use(cors())
-app.use("/testAPI", testAPIRouter)
 
-app.get("/", (req, res) => {
-    res.send("Hello node")
-})
+// app.set('view engine', 'jade')
 
-// io.on('connection', newConnection)
+// app.use(express.static(__dirname + './../client/dist'))
 
-// function newConnection(socket) {
-//     console.log('New connection id= ' + socket.id)
-// }
+// app.get("/", (req, res) => {
+//     console.log(__dirname)
+//     res.render(__dirname + "./../client/dist/index.html")
+// })
+
+io.on('connection', newConnection)
+
+function newConnection(socket) {
+    console.log('New connection id= ' + socket.id)
+}
 
 const listener = server.listen(process.env.PORT || 3030, () => {
     console.log("Node is listening on port: " + listener.address().port)
