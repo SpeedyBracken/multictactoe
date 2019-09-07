9e4e4b87e00b99b1c6a3b96273033f034d864e7b
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-09-06 23:06:29 -0300

    Change global.scss name

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import App from './App'
 
 import 'antd/dist/antd.css'
-import './global.scss'
+import './index.scss'
 
 ReactDOM.render(
     <BrowserRouter basename={process.env.PUBLIC_URL}>
