8e68ac3f8c2e63b4541e2ca20fbe9403e85d9b52
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-09-06 21:57:35 -0300

    Init login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import Zoom from 'react-reveal/Zoom'
 
+import Logo from '../assets/logo_white.png'
+
+import './Login.scss'
+
 export default class Login extends Component{
     constructor(props){
         super(props)
@@ -10,7 +14,13 @@ export default class Login extends Component{
     render(){
         return (
             <Zoom top>
-                <h1>EAE</h1>
+                <div style={{ display: "flex", height: "100vh", width: "100vw", flexDirection: "column" }}>
+                    <img src={Logo} />
+                    <form className="login-container">
+                        <input placeholder='Nickname' />
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
             </Zoom>
         )
     }
