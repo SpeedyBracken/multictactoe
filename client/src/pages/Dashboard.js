7d5776e581e6f03f70dbdbe368a2ae7d88300242
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-09-07 00:05:36 -0300

    Add dashboard elements

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
+import Fade from 'react-reveal/Fade'
 
 export default class Dashboard extends Component{
     constructor(props){
         super(props)
-        this.state = {}
+        this.state = {
+            rooms: [],
+            title: '',
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(){
+        let rooms = this.state.rooms
+        rooms.unshift(title)
+
+        this.setState({ rooms })
     }
 
     render(){
-        return <div />
+        return (
+            <Fade>
+                <div className="dashboard-container">
+                    <form onSubmit={this.handleSubmit} className="form-container">
+                        <input placeholder="Room name" />
+                        <button type="submit">Create Room</button>
+                    </form>
+                </div>
+            </Fade>
+        )
     }
 }
