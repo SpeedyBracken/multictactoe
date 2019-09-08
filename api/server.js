7e38e9303cba681914604cc185fcd2136a309f64
coutlcdo <coutlcdo@outlook.com>
2019-09-07 23:13:03 -0300

    Added basic websocket connections

--- api/server.js
+++ api/server.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
-// const cors = require('cors')
 
-const testAPIRouter = require("./routes/testAPI.js")
+const index = require("./routes/index.js")
 
+app.use(index)
 
 // app.set('view engine', 'jade')
 
@@ -16,11 +16,32 @@ const testAPIRouter = require("./routes/testAPI.js")
 //     res.render(__dirname + "./../client/dist/index.html")
 // })
 
+let arrRooms = []
+
 io.on('connection', newConnection)
 
 function newConnection(socket) {
     console.log('New connection id= ' + socket.id)
+
+    socket.on('newRoom', (data) => {
+        arrRooms.unshift(data)
+        console.log("A new room was created", data)
+    })
+
+    io.emit('tableOfRooms', arrRooms)
+
+    socket.on('disconnect', () => console.log("Client disconnected"))
 }
+
+// const listenRoomCreation = () => {
+//     try {
+//         socket.on('newRoom', () => {
+//             console.log("A new room was created")
+//         })
+//     } catch (error) {
+//         console.error(`Error: ${error.code}`)
+//     }
+// }
 
 const listener = server.listen(process.env.PORT || 3030, () => {
     console.log("Node is listening on port: " + listener.address().port)
